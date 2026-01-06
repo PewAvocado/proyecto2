d0febf9d5930ade91b1dc9a548663dfb87dc66cb
Alvaro Ruiz <ar102rc@gmail.com>
2026-01-06 11:14:36 -0500

    Toda la logica del programa, sobre todo crear un par de funciones y demas

--- javascript.js
+++ javascript.js
@@ -1,7 +1,43 @@
-const contenedor = document.querySelector("#contenedor"); 
+const contenedor = document.querySelector("#contenedor");
+const btnDimension = document.querySelector("#dimension");
 
-for (let i = 0; i < 256; i++) {
-    const cuadro = document.createElement('div');
-    cuadro.classList.add('cuadro'); 
-    contenedor.appendChild(cuadro);
+
+function obtenerColorAleatorio() {
+    const r = Math.floor(Math.random() * 256); 
+    const g = Math.floor(Math.random() * 256); 
+    const b = Math.floor(Math.random() * 256); 
+    
+    return `rgb(${r}, ${g}, ${b})`;
 }
+
+function crearCuadricula(tamano) {
+  
+    contenedor.innerHTML = "";
+
+    contenedor.style.gridTemplateColumns = `repeat(${tamano}, 1fr)`;
+    contenedor.style.gridTemplateRows = `repeat(${tamano}, 1fr)`;
+
+    for (let i = 0; i < tamano * tamano; i++) {
+        const cuadro = document.createElement('div');
+        cuadro.classList.add('cuadrito');
+        
+        cuadro.addEventListener('mouseenter', () => {
+            cuadro.style.backgroundColor = obtenerColorAleatorio();
+        },{once : true});
+
+        contenedor.appendChild(cuadro);
+    }
+}
+
+btnDimension.addEventListener("click", () => {
+    let entrada = prompt("¿Cuántos cuadros por lado quieres? (Máximo 50)", "16");
+    let numero = parseInt(entrada);
+
+    if (numero >= 1 && numero <= 50) {
+        crearCuadricula(numero);
+    } else {
+        alert("Por favor ingresa un número válido entre 1 y 50.");
+    }
+});
+
+crearCuadricula(16);
